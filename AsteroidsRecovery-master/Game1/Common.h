8d8c79c064d4e3e1ef4ef0958a07d33ad0311813
Shatabdi-Barua <48364966+Shatabdi-Barua@users.noreply.github.com>
2020-03-19 10:44:31 +0600

    Initial commit

---
+++ AsteroidsRecovery-master/Game1/Common.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef __common_h__
+#define __common_h__
+
+#include<ctime>
+#include <stdlib.h>
+#include <math.h>
+#include<windows.h>
+#include<GL/gl.h>
+#include<GL/glu.h>
+#include "glut.h"
+
+#include "Constants.h"
+
+#endif /* __common_h__ */
